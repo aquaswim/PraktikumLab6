2bb428085a6455a005ced08472aeb1cc0ccd6e45
Bayu Setiawan <bayu.9183@student.amikom.ac.id>
2017-09-29 14:49:24 +0700

    selesai praktikum 2

--- MyApplication2/app/src/main/java/com/example/amikom/bayuss/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/amikom/bayuss/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.amikom.bayuss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    Button btnTest;
+
+    public void wedus(View view) {
+        Toast.makeText(MainActivity.this, "Wedus", Toast.LENGTH_SHORT).show();
     }
 }
