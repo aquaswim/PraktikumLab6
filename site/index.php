eb4b78a3b3cdd5ca905856ce372b750522bf8a6e
Bayu Setiawan <bayu.9183@student.amikom.ac.id>
2017-09-29 13:17:30 +0700

    bubar prak 1 web

--- site/index.php
+++ site/index.php
@@ -2,10 +2,13 @@
 <html>
 <head>
 	<title>Test</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
 </head>
 <body>
 <?php
 	phpinfo();
 ?>
+<script type="text/javascript" src="/assets/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="/assets/js/bootstrap.js"></script>
 </body>
 </html>
